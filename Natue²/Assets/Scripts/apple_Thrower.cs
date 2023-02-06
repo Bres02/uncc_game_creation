366949f411f5dd1f2d3d1a6ab737826d8fd077e1
Bres02 <blaineevey@gmail.com>
2023-02-06 14:50:36 -0500

    basic enemy attack

--- Natue²/Assets/Scripts/apple_Thrower.cs
+++ Natue²/Assets/Scripts/apple_Thrower.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class apple_Thrower : MonoBehaviour
 {
+    [SerializeField] private GameObject apple;
     public float min = 2f;
     public float max = 3f;
-    private float nextActionTime = 0.0f;
-    public float period = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
         min = transform.position.x;
         max = transform.position.x + 20;
+        InvokeRepeating("ThrowApple",2.0f,4.0f);// throws an apple every x seconds
     }
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextActionTime)
-        {
-            nextActionTime += period;
-            ThrowApple();
-        }
-
-        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z);//moves back and forth
     }
     void ThrowApple()
     {
-
+        Instantiate(apple, transform.position, Quaternion.identity);// creates the apple
     }
 }
